eb7a2d5ebee6f1c9532f06a9d42eebb7cf2d940d
Eduardo Silva <eduardo__lps@hotmail.com>
2019-03-31 20:32:16 +0100

    Documentation added to GameNode.

--- src/game/GameNode.java
+++ src/game/GameNode.java
@@ -4,12 +4,35 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 import heuristic.*;
 
+/**
+ * Represents an instance of a node in a search graph of a solution for a level in the game.
+ */
 public class GameNode extends Node 
 {
+    /**
+     * The state of the board in this node.
+     */
     private GameBoard board;
+
+    /**
+     * The number of moves performed to get to this node.
+     */
     private int moves;
+
+    /**
+     * The number of nodes analyzed in the graph.
+     */
     public static int analyzedNodes = 0;
 
+    /**
+     * Constructor of the class with most members.
+     * @param parentNode The parent node of this node.
+     * @param depth The depth at which this node is at.
+     * @param pathCost The path cost to reach this node.
+     * @param operator This node's operator.
+     * @param moves The number of mobes performed to get to this node.
+     * @param board The state of the board in this node.
+     */
     public GameNode(Node parentNode, int depth, int pathCost, String operator, int moves, GameBoard board)
     {
         super(parentNode, depth, pathCost, operator);
@@ -19,6 +42,14 @@ public class GameNode extends Node
         this.heuristic.calculate(board);
     }
 
+    /**
+     * Constructor of the class with only a few members, the ones remaining being instantiated automatically as if
+     * this node was a regular child of the parent node indicated.
+     * @param parentNode The parent node of this node.
+     * @param operator This node's operator.
+     * @param moves The number of mobes performed to get to this node.
+     * @param board The state of the board in this node.
+     */
     public GameNode(Node parentNode, String operator, int moves, GameBoard board)
     {
         super(parentNode, operator);
@@ -28,6 +59,10 @@ public class GameNode extends Node
         this.heuristic.calculate(board);
     }
 
+    /**
+     * Expands a node, i.e, returns its possible children.
+     * @return The children of this node.
+     */
     public ArrayList<Node> expandNode()
     {
         ArrayList<Node> nodeList = new ArrayList<Node>();
@@ -71,6 +106,10 @@ public class GameNode extends Node
         return nodeList;
     }
 
+    /**
+     * Performs a deapth first search in this node.
+     * @return Flag indicating if the search was successfull or not.
+     */
     public boolean depthSearch()
     {
         if(this.moves >= board.getMaxMoves())
@@ -99,6 +138,10 @@ public class GameNode extends Node
         }
     }
 
+    /**
+     * Performs an iterative deapth first search in this node.
+     * @return Flag indicating if the search was successfull or not.
+     */
     public boolean iterativeDepthSearch() 
     {
         int maxDepth = board.getMaxMoves();
@@ -117,6 +160,10 @@ public class GameNode extends Node
         return false;
     } 
 
+    /**
+     * Auxiliary function to the iterative depth first search.
+     * @return Flag indicating if the search was successfull or not.
+     */
     public boolean depthLimitedSearch(int depth) 
     {
         if(this.depth == depth)
@@ -145,11 +192,13 @@ public class GameNode extends Node
         return false;
     }
 
-    public boolean greedySearch() {
-
+    /**
+     * Performs a greedy search on this node.
+     * @return Flag indicating if the search was successfull or not.
+     */
+    public boolean greedySearch() 
+    {
         PriorityQueue<Node> children = new PriorityQueue<Node>();
-
-        
 
         if(this.testGoal()) {
 
@@ -180,6 +229,10 @@ public class GameNode extends Node
 
     }
 
+    /**
+     * Tests if the goal of game has been accomplished in this node.
+     * @return True if the game has been won and false otherwise.
+     */
     public boolean testGoal()
     {
         char[][] board = getBoard();
@@ -194,6 +247,12 @@ public class GameNode extends Node
         return true;
     }
 
+    /**
+     * Moves a piece in the board.
+     * @param direction The direction in which to move the piece.
+     * @param pieceCoords The coordinates of the piece.
+     * @return A new node representing the board if the move was succesfull or null if not. 
+     */
     public GameNode move(String direction, int[] pieceCoords)
     {
         GameBoard newBoard = null;
@@ -225,6 +284,12 @@ public class GameNode extends Node
         return new GameNode(this, op, this.moves + 1, newBoard);
     }
 
+    /**
+     * Switches a block with another one.
+     * @param direction The direction in which to switch the block.
+     * @param pieceCoords The piece's coordinates.
+     * @return A new node representing the board if the move was succesfull or null if not.
+     */
     public GameNode switchBlock(String direction, int[] pieceCoords)
     {
         GameBoard newBoard = null;
@@ -266,6 +331,9 @@ public class GameNode extends Node
         return new GameNode(this, op, this.moves + 1, newBoard);
     }
 
+    /**
+     * Executes the moves indicated by the operators in the Solution array.
+     */
     public void traceSolution()
     {
         String[] move;
@@ -300,6 +368,9 @@ public class GameNode extends Node
         }
     }
 
+    /**
+     * Adds the correspondent node's operators to the Solution array all the way from the final/acceptance node.
+     */
     public void traceSolutionUp()
     {
         if(!operator.equals("root"))
@@ -309,26 +380,44 @@ public class GameNode extends Node
         }
     }
 
+    /**
+     * Prints the current board.
+     */
     public void printBoard()
     {
         board.printBoard();
     }
 
+    /**
+     * Retrieves the current game board.
+     * @return The current game board.
+     */
     public GameBoard getGameBoard()
     {
         return this.board;
     }
 
+    /**
+     * Retrieves the matrix instance of the board.
+     * @return The matrix representing the actual board.
+     */
     public char[][] getBoard()
     {
         return board.getBoard();
     }
 
+    /**
+     * Sets the current board.
+     * @param board The new board.
+     */
     public void setGameBoard(GameBoard board)
     {
         this.board = board;
     }
 
+    /**
+     * Sets this node heuristic's engine.
+     */
     public void setHeuristic() 
     {
         switch(Heuristic.getCurrentHeuristic()) {
